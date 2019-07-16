6f4dea5320742349c32dc262743508d18eeb362f
Daniel E Calderon <Daniel.Calderon06@gmail.com>
2019-07-16 17:33:03 -0400

    Add models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Question, Answer, Star
 
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Star)
