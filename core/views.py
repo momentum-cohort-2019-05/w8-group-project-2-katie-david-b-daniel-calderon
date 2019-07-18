8fcfa7922a44a97e16a63aa97dbed76d0ab92955
David Barnes <david.barnes115@gmail.com>
2019-07-18 15:08:13 -0400

    fixed user bug on new_question page

--- core/views.py
+++ core/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import get_object_or_404, redirect
 from .forms import AddQuestion
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -36,10 +37,11 @@ def question_detail(request, pk):
    return render(request, 'core/question_detail.html', context=context)
 
 def create_question(request):
+    author = request.user
     if request.method == 'POST':
         form = AddQuestion(request.POST)
         if form.is_valid():
-            question = Question.objects.create(title=form.cleaned_data['title'])
+            question = Question.objects.create(author=author, title=form.cleaned_data['title'])
             question.save()
             return HttpResponseRedirect(reverse('question-detail', args=[question.pk]))
     else:
@@ -47,6 +49,7 @@ def create_question(request):
 
     context = {
         'form': form,
+        'author': author,
     }
 
     return render(request, 'core/new_question.html', context)
