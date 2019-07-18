900fa5829df804cfddfe6d04bb0a44f7bde48ff1
David Barnes <david.barnes115@gmail.com>
2019-07-18 17:33:03 -0400

    updated forms to ModelForms for ques_body

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,13 @@
 from django import forms
+from django.forms import ModelForm
 from core.models import Question, Answer, Star
 
 
-class AddQuestion(forms.Form):
-    title = forms.CharField(max_length=100, help_text='Enter a title for the question.')
-    question = forms.CharField(widget=forms.Textarea, help_text='Enter a description of your question.')
-    
+# class AddQuestion(forms.Form):
+#     title = forms.CharField(max_length=100, help_text='Enter a title for the question.')
+#     question = forms.CharField(widget=forms.Textarea, help_text='Enter a description of your question.')
+
+class AddQuestionForm(ModelForm):
+    class Meta:
+        model = Question
+        fields = ['title', 'ques_body', 'author']
--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from core.models import Question, Answer, Star
 from django.views import generic
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect
-from .forms import AddQuestion
+from .forms import AddQuestionForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.models import User
@@ -27,10 +27,15 @@ def index(request):
 def question_detail(request, pk):
    question = get_object_or_404(Question, pk=pk)
    answers = Answer.objects.filter(question_answered=question)
+   answer = Answer.objects.all()
+#    question_list = Question.objects.all()
+
 
    context = {
        'question': question,
        'answers': answers,
+       'answer': answer,
+    #    'question_list': question_list,
 
    }
 
@@ -39,13 +44,14 @@ def question_detail(request, pk):
 def create_question(request):
     author = request.user
     if request.method == 'POST':
-        form = AddQuestion(request.POST)
+        form = AddQuestionForm(request.POST)
         if form.is_valid():
-            question = Question.objects.create(author=author, title=form.cleaned_data['title'])
-            question.save()
+            # question = Question.objects.create(author=author, title=form.cleaned_data['title'])
+            # question.save()
+            question = form.save()
             return HttpResponseRedirect(reverse('question-detail', args=[question.pk]))
     else:
-        form = AddQuestion()
+        form = AddQuestionForm()
 
     context = {
         'form': form,
