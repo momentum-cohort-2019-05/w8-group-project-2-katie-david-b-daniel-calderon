a4e75205898acda499aa55bb719e14e312c38bfa
David Barnes <david.barnes115@gmail.com>
2019-07-21 23:17:27 -0400

    updated addanswer - still needs work

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('question_detail/<pk>', views.question_detail, name='question-detail'),
     path('new_question/', views.create_question, name='new-question'),
     path('user_profile/<int:pk>', views.user_profile, name='user-profile'),
+    # path('user_profile/<int:pk>', views.UserProfileView.as_view(), name='user-profile'),
     path('delete-question/<int:pk>', views.delete_question, name='delete-question'),
     path('add_answer/', views.add_answer, name='add-answer'),
 
